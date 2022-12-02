9495204d571ff00634ccc8f391d8a1487a0fd8ab
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-12-02 00:13:00 -0800

    Add App.test.js update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// 👇️ wrap component that uses useLocation in Router
+
+test('renders react component', async () => {
+  render(
+    <Router>
+      <App />
+    </Router>,
+  );
+
+  // your tests...
 });
